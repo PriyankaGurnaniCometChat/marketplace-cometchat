342efac0738be93b5539879c310d544ed7ad0764
Igor Silveira <igor.silveira@edirectinsure.com>
2021-04-06 22:35:28 +0100

    Removed new group button

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -4,9 +4,12 @@ import { CometChatGroupListWithMessages } from '../CometChatWorkspace/src';
 
 const InboxPage = () => {
   return (
-    <div className="w-full" style={{
-      height: '90vh'
-    }}>
+    <div
+      className="w-full"
+      style={{
+        height: '90vh',
+      }}
+    >
       <CometChatGroupListWithMessages />
     </div>
   );
